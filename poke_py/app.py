8161fe0ec9ad94dee5d89d0029cf2989071d7103
Vladimir Savin <vladimir@zerc.cc>
2021-12-26 13:04:54 +0000

    Added the homepage.

--- poke_py/app.py
+++ poke_py/app.py
@@ -2,8 +2,7 @@ from __future__ import annotations
 
 from os import environ
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, url_for
 from providers import funtransapi, pokeapi
 
 app = Flask(__name__)
@@ -24,6 +23,16 @@ def get_pokemon_translated(name: str):
         pokemon.description = funtransapi.as_shakespeare(pokemon.description)
 
     return jsonify(pokemon)
+
+
+@app.route("/")
+def index():
+    return jsonify(
+        {
+            "pokemon": url_for("get_pokemon", name="mewtwo"),
+            "pokemon_translated": url_for("get_pokemon_translated", name="mewtwo"),
+        }
+    )
 
 
 if __name__ == "__main__":
