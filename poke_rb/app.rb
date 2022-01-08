6da38f9b373542ac2f9824de28c466b91c78bc27
Vladimir Savin <vladimir@zerc.cc>
2022-01-08 22:25:17 +0000

    Implemented the second route.
    Made the integrational tests pass.

--- poke_rb/app.rb
+++ poke_rb/app.rb
@@ -10,6 +10,19 @@ get '/pokemon/:name' do |name|
   json(get_pokemon(name))
 end
 
+get '/pokemon/translated/:name' do |name|
+  pokemon = get_pokemon(name)
+  description = pokemon[:description]
+
+  pokemon[:description] = if pokemon[:habitat] == 'cave' || pokemon[:isLegendary]
+                            as_yoda(description)
+                          else
+                            as_shakespeare(description)
+                          end
+
+  json(pokemon)
+end
+
 def get_pokemon(name)
   base = ENV['POKEAPI_BASE_URL'] || 'https://pokeapi.co/api/v2'
   data = request_get("#{base}/pokemon-species/#{name}/")
@@ -19,6 +32,18 @@ def get_pokemon(name)
     name: data['name'],
     description: description,
     habitat: data['habitat'] ? data['habitat']['name'] : '',
-    isLegendary: (data['is_legendary'] == 'True')
+    isLegendary: data['is_legendary']
   }
+end
+
+def as_yoda(text)
+  base = ENV['FUNTRANS_BASE_URL'] || 'https://api.funtranslations.com/translate'
+  data = request_post("#{base}/yoda.json", { "text": text })
+  data['contents']['translated']
+end
+
+def as_shakespeare(text)
+  base = ENV['FUNTRANS_BASE_URL'] || 'https://api.funtranslations.com/translate'
+  data = request_post("#{base}/shakespeare.json", { "text": text })
+  data['contents']['translated']
 end
--- poke_rb/requestor.rb
+++ poke_rb/requestor.rb
@@ -9,3 +9,13 @@ def request_get(url)
   response = Net::HTTP.get_response(uri)
   JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
 end
+
+def request_post(url, payload)
+  uri = URI(url)
+  port = uri.port == 443 ? nil : uri.port
+  http = Net::HTTP.new(uri.host, port)
+  request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+  request.body = payload.to_json
+  response = http.request(request)
+  JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
+end
